da53d069503eff9227abe97f67a73fa7ce4f1ab9
Gabriel <gmfaria6@gmail.com>
2022-06-20 22:18:30 -0300

    adding new idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 
 def random_individual_gen(p):
     key1 = ""
@@ -7,6 +8,12 @@ def random_individual_gen(p):
         temp = str(random.randint(0, 1))
         key1 += temp
     return (key1)
+
+
+def calculate_FO(x, y):
+    F6 = 0.5 - ((math.sin(math.sqrt(x*x + y*y)))**2 - 0.5)/((1.0 + 0.001 * (x**2 + y**2))**2)
+
+    return F6
 
 
 def my_AG(population_size=10):
@@ -18,14 +25,22 @@ def my_AG(population_size=10):
         new_individual = np.binary_repr(random.randrange(-100, 100), width=22) +\
                          np.binary_repr(random.randrange(-100, 100), width=22)
 
-        # changing the first string element
-        # new_individual = new_individual[:0] + 'A' + new_individual[0 + 1:]
-        population[generation].append(new_individual)
+        # BEFORE APLYING THE FO
+        x = (int(new_individual[0:21], 2) * (200 / (2 ** 22 - 1))) - 100
+        y = (int(new_individual[21:44], 2) * (200 / (2 ** 22 - 1))) - 100
+
+        population[generation].append({'id': new_individual, 'fitness': calculate_FO(x, y)})
 
     print(population)
 
-    # BEFORE APLYING THE FO
-    # new_individual = (int(new_individual[0:21], 2) * (200/(2**22-1))) - 100
+
+    # changing the first string element
+    # new_individual = new_individual[:0] + 'A' + new_individual[0 + 1:]
+
+    # SELECIONA CONJUNTO DE PAIS (N/2 PARES) - ROLETA
+    # SE DER CRUZAMENTO - CRUZA GERANDO 2 FILHOS - TAXA DE CROSSOVER
+    # SE DER MUTAÇÃO - MUTA FILHOS (APENAS FILHOS) - TAXA DE MUTAÇÃO
+    # PROXIMA GERAÇÃO - TODOS OS FILHOS - 1 (MELHOR PAI, NO LUGAR DE 1 FILHO ALEATÓRIO)
 
 
 if __name__ == '__main__':
