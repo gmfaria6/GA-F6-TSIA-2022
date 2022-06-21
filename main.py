7aafb0219c009ae67b05615bed5a99c8dcdd0bff
Gabriel Matheus <gmfaria6@gmail.com>
2022-06-21 16:34:15 -0300

    complete v1 code with plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 def random_individual_gen(p):
@@ -36,42 +37,103 @@ def roulette_selection(parents_pool, population_size):
     return new_parents
 
 
-def my_AG(population_size=10):
+def crossover(parent1, parent2):
+    position = random.randrange(0, 22)
+
+    new_parent1 = parent1[0:position] + parent2[position:len(parent1)]
+    new_parent2 = parent2[0:position] + parent1[position:len(parent1)]
+
+    return new_parent1, new_parent2
+
+
+def mutation(parent1, parent2):
+    for i in range(0, len(parent1)):
+        if random.randrange(0, 100) < 2:
+            if parent1[i] == '1':
+                parent1 = parent1[:i] + '0' + parent1[i + 1:]
+            else:
+                parent1 = parent1[:i] + '1' + parent1[i + 1:]
+
+    return parent1, parent2
+
+
+def my_AG(population_size, number_of_generations, considered):
     random.seed(55)
-    generation = 0
     population = {}
-    population[generation] = []
-    for i in range(0, population_size):
-        new_individual = np.binary_repr(random.randrange(-100, 100), width=22) +\
-                         np.binary_repr(random.randrange(-100, 100), width=22)
+    population[0] = []
 
-        # BEFORE APLYING THE FO
-        x = (int(new_individual[0:22], 2) * (200 / (2 ** 22 - 1))) - 100
-        y = (int(new_individual[22:44], 2) * (200 / (2 ** 22 - 1))) - 100
+    for generation in range(0, number_of_generations):
+        print("----------------------------------------------------------------------------------------------")
+        print("Generation", generation)
+        print("----------------------------------------------------------------------------------------------")
+        population[generation + 1] = []
+        if not generation:
+            for i in range(0, int(population_size/2)):
+                new_individual = np.binary_repr(random.randrange(-100, 100), width=22) +\
+                                 np.binary_repr(random.randrange(-100, 100), width=22)
 
-        population[generation].append({"id": new_individual, "fitness": calculate_FO(x, y)})
+                x = (int(new_individual[0:22], 2) * (200 / (2 ** 22 - 1))) - 100
+                y = (int(new_individual[22:44], 2) * (200 / (2 ** 22 - 1))) - 100
 
-    parents_pool = []
+                population[generation].append({"id": new_individual, "fitness": round(calculate_FO(x, y), considered)})
 
-    for cromo in population[generation]:
-        parents_pool.append({"id": cromo["id"][0:22], "fitness": cromo["fitness"]})
-        parents_pool.append({"id": cromo["id"][22:44], "fitness": cromo["fitness"]})
+        value = 0
 
-    selected_parents = roulette_selection(parents_pool, population_size)
+        for cromo in population[generation]:
+            if cromo["fitness"] > value:
+                value = cromo["fitness"]
+                best_cromo = cromo
+        print("BEST CROMO FITNESS", best_cromo["fitness"])
 
-    print(selected_parents)
+        parents_pool = []
 
+        for cromo in population[generation]:
+            parents_pool.append({"id": cromo["id"][0:22], "fitness": cromo["fitness"]})
+            parents_pool.append({"id": cromo["id"][22:44], "fitness": cromo["fitness"]})
 
+        selected_parents = roulette_selection(parents_pool, population_size)
 
+        it = 0
 
-    # changing the first string element
-    # new_individual = new_individual[:0] + 'A' + new_individual[0 + 1:]
+        while it < len(selected_parents):
+            parent1 = selected_parents[it]
+            it += 1
+            parent2 = selected_parents[it]
 
-    # SELECIONA CONJUNTO DE PAIS (N/2 PARES) - ROLETA
-    # SE DER CRUZAMENTO - CRUZA GERANDO 2 FILHOS - TAXA DE CROSSOVER
-    # SE DER MUTAÇÃO - MUTA FILHOS (APENAS FILHOS) - TAXA DE MUTAÇÃO
-    # PROXIMA GERAÇÃO - TODOS OS FILHOS - 1 (MELHOR PAI, NO LUGAR DE 1 FILHO ALEATÓRIO)
+            if random.randrange(0, 100) < 76:
+                parent1["id"], parent2["id"] = crossover(parent1["id"], parent2["id"])
+
+            parent1["id"], parent2["id"] = mutation(parent1["id"], parent2["id"])
+
+            x = (int(parent1["id"], 2) * (200 / (2 ** 22 - 1))) - 100
+            y = (int(parent2["id"], 2) * (200 / (2 ** 22 - 1))) - 100
+
+            population[generation + 1].append({"id": parent1["id"]+parent2["id"], "fitness": round(calculate_FO(x, y), considered)})
+            it += 1
+
+        index_out = random.randrange(0, len(population[generation + 1]))
+        population[generation + 1][index_out] = best_cromo
+
+        generation += 1
+    return population
 
 
 if __name__ == '__main__':
-    my_AG()
+    all_population = my_AG(population_size=200, number_of_generations=400, considered=9)
+
+    x = []
+    y = []
+    c = []
+
+    for generation in all_population:
+        for croma in all_population[generation]:
+            c.append(generation)
+            x.append((int(croma["id"][0:22], 2) * (200 / (2 ** 22 - 1))) - 100)
+            y.append((int(croma["id"][22:44], 2) * (200 / (2 ** 22 - 1))) - 100)
+
+    plt.scatter(x, y, c=c, alpha=1)
+    plt.colorbar(label="Generation", orientation="horizontal")
+    plt.show()
+
+    # (100 - 40) BEST CROMO FITNESS 0.9999993979950594
+    # (10 - 400) BEST CROMO FITNESS 0.9999999988619942
