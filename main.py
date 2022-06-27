6a11ada7672a6214eb9a897540cd349acf8adfb8
Gabriel <gmfaria6@gmail.com>
2022-06-27 19:41:04 -0300

    adding new parameters

--- main.py
+++ main.py
@@ -139,10 +139,10 @@ if __name__ == '__main__':
     ax = fig.add_subplot(projection='3d')
     ax.scatter(x, y, z, c=c, cmap=cm.coolwarm, marker='o')
 
+    plt.savefig("GA_F6.png")
+
     # plt.colorbar(label="Generation", orientation="horizontal", c=c, cmap=cm.coolwarm)
     plt.show()
-
-    plt.save("GA_F6.png")
 
     # (100 - 40) BEST CROMO FITNESS 0.9999993979950594
     # (10 - 400) BEST CROMO FITNESS 0.9999999988619942
